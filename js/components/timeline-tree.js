4e51e5c96a22cc7016d6e41808af53b1d8d50c25
hung <nhuhung19@gmail.com>
2019-11-05 22:45:32 +0700

    Converted from manual to general timeline toggling.

--- js/components/timeline-tree.js
+++ js/components/timeline-tree.js
@@ -1,11 +1,58 @@
 
 function initTimeline(){
-    let btnExp = document.querySelector('#btnExp')   
-    let btnEdu = document.querySelector('#btnEdu')
-    let timelineExp = document.querySelector('#timelineExp')   
-    let timelineEdu = document.querySelector('#timelineEdu')
 
+    // RW
+    // let btnExp = document.querySelector('#btnExp')   
+    // let btnEdu = document.querySelector('#btnEdu')
+    let buttons = document.querySelectorAll('.experiences__button')
+
+    // RW
+    let timelines = document.querySelectorAll('.timeline')
+    // let timelineExp = document.querySelector('#timelineExp')   
+    // let timelineEdu = document.querySelector('#timelineEdu')
+    let btnActive
+   
+    // RW
+    // highlight(btnEdu, btnExp)
+    let firstBtn = buttons[0]
+    highlight(null, firstBtn)
+
+    // RW
+    // hideTree(timelineEdu)
+
+    // RW
+    let firstTimeline = timelines[0]
+    showTree(firstTimeline)
     
+    // RW
+    // btnExp.addEventListener('click', () => {
+    //     highlight(btnEdu,btnExp)
+    //     hideTree(timelineEdu)
+    //     showTree(timelineExp)
+    // })
+    // btnEdu.addEventListener('click', () => {
+    //     highlight(btnExp,btnEdu)
+    //     hideTree(timelineExp)
+    //     showTree(timelineEdu)
+    // })
+    
+    for( let btn of buttons){
+        btn.addEventListener('click', onClick)
+    }
+    function onClick(){
+        let btnNew = event.target
+        // highlight(btnActive, btnNew) // Run incorrectly
+        
+        let strIDActive = btnActive.dataset.show
+        let timelineActive = document.querySelector(`#${strIDActive}`)
+        hideTree(timelineActive)
+        
+        let strIDNew = btnNew.dataset.show
+        let timelineNew = document.querySelector(`#${strIDNew}`)
+        showTree(timelineNew)
+
+        highlight(btnActive, btnNew)
+    }
     
     function activate(btn) {
         btn.classList.add('experiences__button--active')
@@ -14,8 +61,13 @@ function initTimeline(){
         btn.classList.remove('experiences__button--active')
     }
     function highlight(btnOld, btnNew){
-        inactivate(btnOld)
+        if (btnOld != null) {
+            inactivate(btnOld)
+        }
         activate(btnNew)
+
+        // ghi nhat ky
+        btnActive = btnNew
     }
     
     function hideTree(timeline){
@@ -25,21 +77,37 @@ function initTimeline(){
         timeline.classList.remove('d-none')
     }
 
-    highlight(btnEdu,btnExp)
-    hideTree(timelineEdu)
-    showTree(timelineExp)
-    btnExp.addEventListener('click', () => {
-        highlight(btnEdu,btnExp)
-        hideTree(timelineEdu)
-        showTree(timelineExp)
-    })
 
-    btnEdu.addEventListener('click', () => {
-        highlight(btnExp,btnEdu)
-        hideTree(timelineExp)
-        showTree(timelineEdu)
+    // B
+    // let btnExp = document.querySelector('#btnExp')   
+    // let btnEdu = document.querySelector('#btnEdu')
+    // let timelineExp = document.querySelector('#timelineExp')   
+    // let timelineEdu = document.querySelector('#timelineEdu')
 
-    })
+
+    // // B
+    // highlight(btnEdu, btnExp)
+
+    // // B
+    // hideTree(timelineEdu)
+
+    // // B
+    // showTree(timelineExp)
+    
+    // // B
+    // btnExp.addEventListener('click', () => {
+    //     highlight(btnEdu,btnExp)
+    //     hideTree(timelineEdu)
+    //     showTree(timelineExp)
+    // })
+
+    // // B
+    // btnEdu.addEventListener('click', () => {
+    //     highlight(btnExp,btnEdu)
+    //     hideTree(timelineExp)
+    //     showTree(timelineEdu)
+
+    // })
 
 }
     
